421529113354ad643745e6df2b1cabac0893310c
Lixian Zhang <lixian.97.m@gmail.com>
2023-03-26 18:28:19 +0200

    refactor

--- src/autofishing/common.py
+++ src/autofishing/common.py
@@ -1,5 +1,7 @@
-hand_bin_name = 'hand.exe'
-pipe_name = '\\\\.\\pipe\\stardrew_auto_fishing_pipe'
+# compile with 'clang Hand.c -std=c99 -o hand.exe' and move the output to the right place
+hand_bin_name = 'hand.exe' 
+# make sure this one is identical to PIPE_NAME in Hand.c
+pipe_name = '\\\\.\\pipe\\stardrew_auto_fishing_pipe' 
 
 fish_pattern_name = 'fish.npy'
 fishing_UI_pattern_name = 'fishing_UI.npy'
