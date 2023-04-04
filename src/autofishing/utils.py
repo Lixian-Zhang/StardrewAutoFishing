f35e33ed579ea18f8b61ef2654b9479e16ad995c
Lixian Zhang <lixian.97.m@gmail.com>
2023-04-04 21:32:26 +0200

    refactor

--- src/autofishing/utils.py
+++ src/autofishing/utils.py
@@ -15,6 +15,9 @@ def _window_enum_handler(hwnd, results: list) -> None:
 def get_fishing_level():
     return 10
 
+def get_fishing_bar_length():
+    return get_fishing_level() * 6 + 72
+
 def get_repo_root_path():
     return Path(__file__).parent.parent.parent
 
