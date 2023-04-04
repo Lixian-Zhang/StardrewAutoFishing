42c192519904eeb68606af07e99ecc2752be3b32
Lixian Zhang <lixian.97.m@gmail.com>
2023-04-04 21:32:59 +0200

    a primitive controller is implemented

--- src/autofishing/Agent.py
+++ src/autofishing/Agent.py
@@ -3,6 +3,7 @@ import win32api
 from time import sleep
 from .Hand import Hand
 from .Eye import Eye
+from .utils import get_fishing_bar_length
 
 class Agent:
 
@@ -14,10 +15,27 @@ class Agent:
         pass
 
     def run(self):
+        last_e = None
+        sum_u = 0
+        kp = -0.05
+        kd = -0.01
+        kiu = -0.15
+
         while True:
             game_info = self.eye.get_game_info()
             if game_info is None:
                 self.hand.stop()
                 continue
             fish_y, fishing_bar_y, progress = game_info
-            self.hand.click(0.50 + (fish_y - fishing_bar_y) / -300 * 1)
+            fish_y = min(fish_y, 444 - get_fishing_bar_length() // 2)
+            fish_y = max(fish_y, 21 + get_fishing_bar_length() // 2)
+            maintaining_duty_ratio = 0.50
+            e = fish_y - fishing_bar_y
+            
+            duty_cycle = maintaining_duty_ratio + kp * e
+            if last_e is not None:
+                duty_cycle += kd * (e - last_e)
+            duty_cycle += sum_u * kiu
+            self.hand.click(duty_cycle)
+            sum_u += duty_cycle - maintaining_duty_ratio
+            last_e = e
