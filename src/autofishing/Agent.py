ecc1fd769f88058d2f6bff323a8a9859c963761a
Lixian Zhang <lixian.97.m@gmail.com>
2023-04-05 23:29:51 +0200

    Tweaked controller parameter. It can  beat legendary  fish now.

--- src/autofishing/Agent.py
+++ src/autofishing/Agent.py
@@ -14,27 +14,33 @@ class Agent:
     def run(self):
         last_e = None
         sum_u = 0
+        sum_e = 0
         kp = -0.05
-        kd = -0.01
-        kiu = -0.15
+        kd = -0.02
+        ki = -0.002
+        kiu = -0.4
 
         while True:
             game_info = self.eye.get_game_info()
             if game_info is None:
                 last_e = None
                 sum_u = 0
+                sum_e = 0
                 self.hand.stop()
                 continue
             fish_y, fishing_bar_y, progress = game_info
             fish_y = min(fish_y, 444 - get_fishing_bar_length() // 2)
             fish_y = max(fish_y, 21 + get_fishing_bar_length() // 2)
-            maintaining_duty_ratio = 0.50
+            maintaining_duty_ratio = 0.52
             e = fish_y - fishing_bar_y
             
             duty_cycle = maintaining_duty_ratio + kp * e
             if last_e is not None:
                 duty_cycle += kd * (e - last_e)
             duty_cycle += sum_u * kiu
+            duty_cycle += sum_e * ki
+
             self.hand.click(duty_cycle)
             sum_u += duty_cycle - maintaining_duty_ratio
+            sum_e += e
             last_e = e
